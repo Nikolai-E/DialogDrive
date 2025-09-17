c033e481fa90987ba20d454814c603e6995a200a
Nikolai-E <nikolai.e@live.no>
2025-09-17 16:10:53 +0200

    removed unsued code

--- entrypoints/popup/hooks/useWorkspaceSelector.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export const useWorkspaceSelector = (all: string[], initial: string = 'General') => {
-  const [workspace, setWorkspace] = React.useState<string>(initial);
-  const options = React.useMemo(() => Array.from(new Set(['General', ...all])), [all]);
-  return { workspace, setWorkspace, options };
-};
